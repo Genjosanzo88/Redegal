97e8cd16e218933588228e4531b3a78ae4b0a90c
francisco.quiros <francisco.quiros@conjurer.es>
2023-01-18 17:01:10 +0100

    api call with hook

--- src/products/pages/Products.jsx
+++ src/products/pages/Products.jsx
@@ -1,6 +1,54 @@
+import { useAxios } from "../hooks";
+import { Link } from 'react-router-dom';
+
+import './Products.css';
 
 export const Products = () => {
-  return (
-    <h1>Productos nene 1</h1>
-  )
-}
+    const { response, loading, error } = useAxios({
+        method: 'GET',
+        url: '/api/characters',
+    });
+
+    return (
+        <div className='App'>
+            <h1>Characters</h1>
+
+            {loading ? (
+                <p>Loading...</p>
+            ) : (
+                <div>
+                    {error && (
+                        <div>
+                            <p>{error.message}</p>
+                        </div>
+                    )}
+                     
+                      { response.map(response => 
+                        <Link to="#" className="my-card">
+                          <img src={response.image} className="img img-responsive" alt={response.name}/>
+                          <div className="profile-name">{response.name}</div>
+                          {/* <div className="profile-position">{alter_ego}</div>
+                          <div className="profile-overview">
+                              <div className="profile-overview">
+                                  <div className="row">
+                                      <div className="col-ms-4">
+                                          <h3>{publisher}</h3>
+                                          <p>Primera aparición: <br />{first_appearance}</p>
+                                          {
+                                              (alter_ego !== characters)
+                                              && <p>{characters}</p>
+                                          }
+                                      </div>
+                                  </div>
+                              </div>
+                          </div> */}
+                      </Link>
+                      
+                      ) }
+                   
+                    
+                </div>
+            )}
+        </div>
+    );
+};
