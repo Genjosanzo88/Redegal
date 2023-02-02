2bae1895e4bcd8e92e38a5cf7e665381397b3c01
francisco.quiros <francisco.quiros@conjurer.es>
2023-02-02 16:58:01 +0100

    Estilos

--- src/products/hooks/useAxios.js
+++ src/products/hooks/useAxios.js
@@ -4,24 +4,33 @@ import axios from 'axios';
 axios.defaults.baseURL = 'https://itunes.apple.com';
 
 export const useAxios = (axiosParams) => {
-    const [response, setResponse] = useState(undefined);
-    const [error, setError] = useState('');
-    const [loading, setLoading] = useState(true);
+  const [response, setResponse] = useState(undefined);
+  const [error, setError] = useState('');
+  const [loading, setLoading] = useState(true);
 
-    const fetchData = async (params) => {
+  const fetchData = async (params) => {
+      const cachedData = localStorage.getItem(params.url);
+      const now = new Date().getTime();
+      if (cachedData && now - JSON.parse(cachedData).timestamp < 24 * 60 * 60 * 1000) {
+          setResponse(JSON.parse(cachedData).data);
+          setLoading(false);
+          return;
+      }
+
       try {
-       const result = await axios.request(params);
-       setResponse(result.data);
-       } catch( error ) {
-         setError(error);
-       } finally {  
-         setLoading(false);
-       }
-    };
+          const result = await axios.request(params);
+          setResponse(result.data);
+          localStorage.setItem(params.url, JSON.stringify({ data: result.data, timestamp: now }));
+      } catch (error) {
+          setError(error);
+      } finally {
+          setLoading(false);
+      }
+  };
 
-    useEffect(() => {
-        fetchData(axiosParams);
-    }, []); // execute once only
+  useEffect(() => {
+      fetchData(axiosParams);
+  }, []); // execute once only
 
-    return { response, error, loading };
+  return { response, error, loading };
 };
--- src/products/pages/Products.jsx
+++ src/products/pages/Products.jsx
@@ -1,13 +1,12 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useAxios } from '../hooks';
 import { Link } from 'react-router-dom';
 import defaultImage from '../../assets/Noi.jpg';
 import './Products.css';
 
-
 export const Products = () => {
-  const [searchTerm, setSearchTerm] = React.useState('');
-  const [filteredPodcasts, setFilteredPodcasts] = React.useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [filteredPodcasts, setFilteredPodcasts] = useState([]);
   const { response, loading, error } = useAxios({
     method: 'GET',
     url: '/us/rss/toppodcasts/limit=100/genre=1310/json',
@@ -15,7 +14,11 @@ export const Products = () => {
 
   useEffect(() => {
     if (response) {
-      setFilteredPodcasts(response.feed.entry.filter(item => item['im:name'].label.toLowerCase().includes(searchTerm.toLowerCase())));
+      const filteredResults = response.feed.entry.filter(item => 
+        item['im:name'].label.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        item['im:artist'].label.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+      setFilteredPodcasts(filteredResults);
     }
   }, [response, searchTerm]);
 
@@ -32,11 +35,13 @@ export const Products = () => {
       <h1>Podcasts</h1>
       <input type="text" placeholder="Search podcasts" value={searchTerm} onChange={e => setSearchTerm(e.target.value)} />
       { filteredPodcasts.length > 0 ? (
-        <div>
+        <div> 
+          <p>{filteredPodcasts.length} podcast(s) found</p>
           { filteredPodcasts.map((item, index) => (
-            <Link key={index} to="#" className="my-card">
+            <Link key={index} to="#" className="my-card" style={{ textDecoration: 'none' }}>
               <img src={item['im:image'][0].label || defaultImage} className="img img-responsive" alt={item['im:name'].label} />
               <div className="profile-name">{item['im:name'].label}</div>
+              <div >Author: {item['im:artist'].label}</div>
             </Link>
           ))}
         </div>
